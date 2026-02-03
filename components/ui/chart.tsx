50bf99a882f340f2005c2b77a92a93ab6d667c67
Julia Hadja <juliahadja@gmail.com>
2026-02-03 01:28:25 -0300

    Fix: legenda gráficos

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -272,7 +272,7 @@ function ChartLegendContent({
   return (
     <div
       className={cn(
-        "flex items-center justify-center gap-4",
+        "flex items-center justify-center gap-2 flex-wrap",
         verticalAlign === "top" ? "pb-3" : "pt-3",
         className
       )}
